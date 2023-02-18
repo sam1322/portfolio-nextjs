0446dae6ca97661b3b62c4fbf51200ef7d9224cf
Sriram <sriram.pant5@gmail.com>
2023-02-18 16:12:49 +0530

    completed the main component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import React from "react";
+import React, {useState} from "react";
 import navLogo from "public/assets/navLogo.png";
 import Link from "next/link";
-import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai";
-
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {BsFillPersonLinesFill} from "react-icons/bs";
 const Navbar = (props) => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100] bg-white">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -27,22 +32,66 @@ const Navbar = (props) => {
               <li className="ml-10 text-sm uppercase hover:border-b ">Contact </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={20} />
           </div>
         </div>
       </div>
-      <div className="fixed left-0 top-0 w-full h-screen  bg-black/70">
-        <div className=" fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] lg:hidden h-screen bg-white p-10 ease-in duration-500 ">
+      <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen  bg-black/70" : ""}>
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] lg:hidden h-screen bg-white p-10 ease-in duration-500 "
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
           <div>
             <div className="flex w-full items-center justify-between">
               <Image src={navLogo} width="60" height="35" alt="Logo" />
-              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                 <AiOutlineClose />
               </div>
             </div>
             <div className="border-b border-gray-300 my-4">
-              <p className="w-[85%] md:w-[90%] py-4">Let&apos;s build something Legendary Together</p>
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let&apos;s build something Legendary Together
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="py-4 text-sm"> Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm"> About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm"> Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">Let&apos;s Connect </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 ">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 ">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 ">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 ">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
             </div>
           </div>
         </div>

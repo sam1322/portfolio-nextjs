8a857b251d193b5bec90c1726eca3742ea46f8c6
Sriram <sriram.pant5@gmail.com>
2023-02-19 14:22:53 +0530

    added project section

--- components/Projects.js
+++ components/Projects.js
@@ -1,14 +1,41 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
+  const projectList = [
+    {
+      title: "Property Finder",
+      backgroundImg: "/../public/assets/projects/property.jpg",
+      // projectUrl: "/property",
+      projectUrl: "/",
+    },
+    {
+      title: "Crypto App",
+      backgroundImg: "/../public/assets/projects/crypto.jpg",
+      projectUrl: "/",
+    },
+    {
+      title: "Netflix App",
+      backgroundImg: "/../public/assets/projects/netflix.jpg",
+      projectUrl: "/",
+    },
+    {
+      title: "Twitch App",
+      backgroundImg: "/../public/assets/projects/twitch.jpg",
+      projectUrl: "/",
+    },
+    
+  ];
   return (
     <div className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase">Projects</p>
-        <h2 className="py-4">What I've Built</h2>
+        <h2 className="py-4">What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff] ] ">hi</div>
+          {projectList.map((item, index) => (
+            <ProjectItem key={index} {...item} />
+          ))}
         </div>
       </div>
     </div>
